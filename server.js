d3e774d81f3924f325b62cf89b2a67c874b2cc17
probinson2015 <porsha_robinson@ymail.com>
2017-05-16 22:30:26 -0700

    express static middleware

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ server.get('/', (req, res) => {
 	res.send('express loaded on get homepage');
 });
 
+// server.use mounts middleware functions at the specified path
+// express.static is the only built in middleware in express
+// It serves static files
+server.use(express.static('public'))
+
+// sending html via fs module and toString()
+// import fs from 'fs'; 
+// server.get('/about.html', (req, res) => {
+// 	fs.readFile('./about.html', (err, data) => {
+// 		res.send(data.toString());
+// 	});
+// });
+
 server.listen(config.port, () => {
 	console.info('Server listening on port: ', config.port);
 });
