74eaf7dc32cd7024876ac3622f26b94dca28d0ba
probinson2015 <porsha_robinson@ymail.com>
2017-05-16 20:28:30 -0700

    adding express

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-//to import a non default export
-//use the destruct syntax
-// import config, { nodeEnv } from './config';
+import config from './config';
 
-import http from 'http';
+import express from 'express';
+const server = express();
 
-//node will automatically attach the req,res argument 
-//to the http request event
 
-const server = http.createServer((req, res) => {
-	res.write('Hello HTTP!\n');
-	setTimeout(() => {
-		//writing to the response stream
-		res.write('I can stream!\n');
-		res.end();
-	}, 3000);
+server.get('/', (req, res) => {
+	res.send('express loaded on get homepage');
 });
 
-server.listen(8080);
-
-// //the request event is emitted every time the server sees the request
-// server.on('request', (req, res) => {
-// 	res.write('Hello HTTP!\n');
-// 	setTimeout(() => {
-// 		//writing to the response stream
-// 		res.write('I can stream!\n');
-// 		res.end();
-// 	}, 3000);
-// });
+server.listen(config.port, () => {
+	console.info('Server listening on port ', config.port);
+});
