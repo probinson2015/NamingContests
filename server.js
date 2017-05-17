0f38e6f6dda8dd6ae3e5213a5f7a4022261ce2a0
probinson2015 <porsha_robinson@ymail.com>
2017-05-16 20:18:10 -0700

    playing with http module

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 //use the destruct syntax
 // import config, { nodeEnv } from './config';
 
-import express from 'express';
-const server = express();
-
+import http from 'http';
 
 //node will automatically attach the req,res argument 
 //to the http request event
-const server = http.createServer(config.port (req, res) => {
+
+const server = http.createServer((req, res) => {
 	res.write('Hello HTTP!\n');
 	setTimeout(() => {
 		//writing to the response stream
@@ -17,7 +16,7 @@ const server = http.createServer(config.port (req, res) => {
 	}, 3000);
 });
 
-
+server.listen(8080);
 
 // //the request event is emitted every time the server sees the request
 // server.on('request', (req, res) => {
